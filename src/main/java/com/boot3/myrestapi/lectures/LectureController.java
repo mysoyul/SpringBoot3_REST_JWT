e059611bfb8eb42a0188eb5324f21889ab9b8755
mysoyul <vega2k_@naver.com>
2023-11-14 17:28:13 +0900

    LectureResouce use

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,6 +1,8 @@
 package com.boot3.myrestapi.lectures;
 
 import com.boot3.myrestapi.lectures.dto.LectureReqDto;
+import com.boot3.myrestapi.lectures.dto.LectureResDto;
+import com.boot3.myrestapi.lectures.dto.LectureResource;
 import com.boot3.myrestapi.lectures.validator.LectureValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping(value = "/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
@@ -43,15 +47,24 @@ public class LectureController {
             return getErrors(errors);
         }
 
+        //ReqDto -> Entity
         Lecture lecture = modelMapper.map(lectureReqDto, Lecture.class);
         //offline, free 값을 update
         lecture.update();
         Lecture addLecture = this.lectureRepository.save(lecture);
+        //Entity -> ResDto
+        LectureResDto lectureResDto = modelMapper.map(addLecture, LectureResDto.class);
 
         WebMvcLinkBuilder selfLinkBuilder =
-                WebMvcLinkBuilder.linkTo(LectureController.class).slash(addLecture.getId());
+                linkTo(LectureController.class).slash(lectureResDto.getId());
         URI createUri = selfLinkBuilder.toUri();
-        return ResponseEntity.created(createUri).body(addLecture);
+
+        LectureResource lectureResource = new LectureResource(lectureResDto);
+        lectureResource.add(linkTo(LectureController.class).withRel("query-lectures"));
+        lectureResource.add(selfLinkBuilder.withSelfRel());
+        lectureResource.add(selfLinkBuilder.withRel("update-lecture"));
+
+        return ResponseEntity.created(createUri).body(lectureResource);
     }
 
     private static ResponseEntity<Errors> getErrors(Errors errors) {
