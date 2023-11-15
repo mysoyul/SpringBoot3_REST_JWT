e0a8d7a5dee1cc704840a2d95bbf2abacf176205
mysoyul <vega2k_@naver.com>
2023-11-15 13:50:32 +0900

    securityfilterchain add

--- src/main/java/com/boot3/myrestapi/security/config/SecurityConfig.java
+++ src/main/java/com/boot3/myrestapi/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -37,7 +38,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
+        return   //http.csrf(AbstractHttpConfigurer::disable)
+                http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/users/welcome").permitAll()
                             .requestMatchers("/api/lectures/**").authenticated();
