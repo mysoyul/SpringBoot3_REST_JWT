63d0d116f202bb99a1eef7cd36f5ab31ef4d9909
mysoyul <vega2k_@naver.com>
2023-11-15 16:32:22 +0900

    user/login add

--- src/main/java/com/boot3/myrestapi/security/userinfo/UserInfoController.java
+++ src/main/java/com/boot3/myrestapi/security/userinfo/UserInfoController.java
@@ -1,6 +1,12 @@
 package com.boot3.myrestapi.security.userinfo;
 
+import com.boot3.myrestapi.security.jwt.JwtService;
+import com.boot3.myrestapi.security.userinfo.dto.AuthRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +17,11 @@ public class UserInfoController {
     private UserInfoRepository repository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,5 +34,19 @@ public class UserInfoController {
                 passwordEncoder.encode(userInfo.getPassword()));
         UserInfo savedUserInfo = repository.save(userInfo);
         return savedUserInfo.getName() + " user added!!";
+    }
+
+    @PostMapping("/login")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()
+                ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
     }
 }
